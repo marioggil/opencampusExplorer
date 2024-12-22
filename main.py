0dafd3ad948f31dbfe60be3d9314275c84bb1a21
marioggil <marioggil@gmail.com>
2024-12-22 15:10:08 -0500

    Restructure Api and new database struture

--- main.py
+++ main.py
@@ -673,7 +673,6 @@ def contractsDetail(hash):
     db.commit() 
     return {"message":"OK"}
 
-
 @app.post("/wallet/{hash}/tx")
 def TxsWallet(request: Request,hash: str) :#-> Tuple[List[Dict], Set[str], bool]:
     """
